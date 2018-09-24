2f02d9bd93b638a5764791dbad01d1ba81d2f484
Mary McKeon <mmckeon204@gmail.com>
2018-09-23 21:07:24 -0400

    updating test file

--- gitRequestTest.py
+++ gitRequestTest.py
@@ -9,7 +9,7 @@ class gitRequestTest(unittest.TestCase):
 		
 	def testGetCommits(self):
 		print("second commit test")
-		self.assertEqual(gitRequest.getCommits("mmckeon16", "GitHubAPI567"), 3)
+		self.assertEqual(gitRequest.getCommits("mmckeon16", "gameStates"), 2)
 		self.assertEqual(gitRequest.getCommits(123, 5.968), "Not valid input")
 		self.assertEqual(gitRequest.getCommits("mmckeon16", "48"), 2)
 		self.assertEqual(gitRequest.getCommits("nfwjfjberfosermf", "fknzefj"), 2)
