7a83396cd0bf398e4c38497cbd2031f884a06821
Mary McKeon <mmckeon204@gmail.com>
2018-09-23 21:05:08 -0400

    updating test file

--- gitRequestTest.py
+++ gitRequestTest.py
@@ -5,14 +5,14 @@ import gitRequest
 class gitRequestTest(unittest.TestCase):
 	def testGetRepos(self):
 		self.assertTrue(gitRequest.getRepos("mmckeon16"))
-		#self.assertFalse(gitRequest.getRepos(123))
+		self.assertFalse(gitRequest.getRepos(123))
 		
 	def testGetCommits(self):
 		print("second commit test")
-	#	self.assertEqual(gitRequest.getCommits("mmckeon16", "GitHubAPI567"), 1)
-	#	self.assertEqual(gitRequest.getCommits(123, 5.968), "Not valid input")
-	#	self.assertEqual(gitRequest.getCommits("mmckeon16", "48"), "No commits, valid user")
-	#	self.assertEqual(gitRequest.getCommits("nfwjfjberfosermf", "fknzefj"), "Not valid repo or user")
+		self.assertEqual(gitRequest.getCommits("mmckeon16", "GitHubAPI567"), 3)
+		self.assertEqual(gitRequest.getCommits(123, 5.968), "Not valid input")
+		self.assertEqual(gitRequest.getCommits("mmckeon16", "48"), 2)
+		self.assertEqual(gitRequest.getCommits("nfwjfjberfosermf", "fknzefj"), 2)
 		
 
 if __name__ == '__main__':
